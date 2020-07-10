458265d3907ccd1424c6b1b2e435bf464e3c61c6
Miguel Felix <mikeyfelix99@gmail.com>
2020-07-10 09:30:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 def speak_to_grandma (phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-    
+    binding.pry
   elsif phrase == phrase.downcase
     puts "HUH? SPEAK UP, SONNY!"
   else phrase == phrase.upcase

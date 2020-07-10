1e1e49ed5755fc8c207a0d7f2bdf7b4ad97f1ccb
Miguel Felix <mikeyfelix99@gmail.com>
2020-07-10 09:40:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 def speak_to_grandma (phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.downcase
+  elsif phrase == phrase.capitalize
     return "HUH? SPEAK UP, SONNY!"
   else phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
